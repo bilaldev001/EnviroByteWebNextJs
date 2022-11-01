822691d587d4b9d7398c627ccfa0fff9fd33d7cd
miao.zhang1 <miao.zhang1@ucalgary.ca>
2022-10-31 22:41:49 -0600

    Use Formspree for contact us page

--- pages/contact.js
+++ pages/contact.js
@@ -1,9 +1,10 @@
 import React from "react";
 import Navbar from "../components/Layouts/Navbar";
 import PageBanner from "../components/Common/PageBanner";
-import ContactInfo from "../components/Contact/ContactInfo";
-import ContactForm from "../components/Contact/ContactForm";
-import SubscribeStyleThree from "../components/Common/SubscribeStyleThree";
+// import ContactInfo from "../components/Contact/ContactInfo";
+// import ContactForm from "../components/Contact/ContactForm";
+import ContactForm from '../components/Contact/freecontact';
+// import SubscribeStyleThree from "../components/Common/SubscribeStyleThree";
 import Footer from "../components/Layouts/Footer";
 
 const Contact = () => {
@@ -19,13 +20,7 @@ const Contact = () => {
         bgImage="/images/page-title-bg.jpg"
       />
 
-      <ContactInfo />
-
       <ContactForm />
-
-      <div className="ptb-100">
-        <SubscribeStyleThree />
-      </div>
 
       <Footer />
     </>

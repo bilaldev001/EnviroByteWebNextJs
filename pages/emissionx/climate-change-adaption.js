f5429da85e63b03e6db34f3219c3e8e326980eed
miao.zhang1 <miao.zhang1@ucalgary.ca>
2022-11-04 18:24:30 -0600

    Add climate change adaption page

---
+++ pages/emissionx/climate-change-adaption.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Navbar from "../../components/Layouts/Navbar";
+import PageBanner from "../../components/Common/PageBanner";
+import ProjectDetailsContent from "../../components/Projects/ProjectDetailsContent";
+import Footer from "../../components/Layouts/Footer";
+
+const ProjectDetails = () => {
+	return (
+		<>
+			<Navbar />
+
+			<PageBanner
+				pageTitle="Climate Change Adaption"
+				breadcrumbTextOne="Home"
+				breadcrumbTextTwo="Climate Change Adaption"
+				breadcrumbUrl="/"
+				bgImage="/images/page-title-bg-3.jpeg"
+			/>
+
+			<ProjectDetailsContent />
+
+			<Footer />
+		</>
+	);
+};
+
+export default ProjectDetails;

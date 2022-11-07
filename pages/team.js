f513acc917d67cdfa99131a78cc76b10cbf7cb54
miao.zhang1 <miao.zhang1@ucalgary.ca>
2022-11-06 21:13:41 -0700

    Add meta data for team

--- pages/team.js
+++ pages/team.js
@@ -3,10 +3,23 @@ import Navbar from "../components/Layouts/Navbar";
 import PageBanner from "../components/Common/PageBanner";
 import TeamMemberContent from "../components/Team/TeamMemberContent";
 import Footer from "../components/Layouts/Footer";
+import Head from "next/head";
 
 const Team = () => {
   return (
     <>
+        <Head>
+            <title>EnviroByte Inc. | Team</title>
+            <meta name="keywords" content={"envirobyte Environment GHG Calgary calgary Alberta Team"}/>
+            <meta property="og:title" content="EnviroByte Inc. - Team"/>
+            <meta property="og:PageType" content="website"/>
+            <meta property="og:type" content="website"/>
+            <meta property="og:site_name" content="https://www.envirobyte.com"/>
+            <meta property="og:url" content="https://www.envirobyte.com/pages/team.js"/>
+            <meta property="og:locale" content="en"/>
+            <meta name="viewport" content="width=device-width,initial-scale=1.0"/>
+            <meta name="description" content="Envirobyte services are here to help you and your business...Official web site of EnviroByte, located in Calgary, Alberta, Canada."/>
+        </Head>
       <Navbar />
 
       <PageBanner

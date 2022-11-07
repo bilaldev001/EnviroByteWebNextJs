4d230f234dc8b4a08bf2ed40474ae631a4a8716f
miao.zhang1 <miao.zhang1@ucalgary.ca>
2022-11-06 21:15:30 -0700

    Delete banner img in contact

--- pages/contact.js
+++ pages/contact.js
@@ -6,25 +6,39 @@ import PageBanner from "../components/Common/PageBanner";
 import ContactForm from '../components/Contact/freecontact';
 // import SubscribeStyleThree from "../components/Common/SubscribeStyleThree";
 import Footer from "../components/Layouts/Footer";
+import Head from "next/head";
 
 const Contact = () => {
-  return (
-    <>
-      <Navbar />
+	return (
+		<>
+			<Head>
+				<title>EnviroByte Inc. | Contact</title>
+				<meta name="keywords" content={"envirobyte Environment GHG Calgary calgary Alberta Contact"}/>
+				<meta property="og:title" content="EnviroByte Inc. - Contact"/>
+				<meta property="og:PageType" content="website"/>
+				<meta property="og:type" content="website"/>
+				<meta property="og:site_name" content="https://www.envirobyte.com"/>
+				<meta property="og:url" content="https://www.envirobyte.com/pages/contact.js"/>
+				<meta property="og:locale" content="en"/>
+				<meta name="viewport" content="width=device-width,initial-scale=1.0"/>
+				<meta name="description"
+					  content="Envirobyte services are here to help you and your business...Official web site of EnviroByte, located in Calgary, Alberta, Canada."/>
+			</Head>
+			<Navbar/>
 
-      <PageBanner
-        pageTitle="Contact Us"
-        breadcrumbTextOne="Home"
-        breadcrumbTextTwo="Contact Us"
-        breadcrumbUrl="/"
-        bgImage="/images/page-title-bg.jpg"
-      />
+			<PageBanner
+				pageTitle="Contact Us"
+				breadcrumbTextOne="Home"
+				breadcrumbTextTwo="Contact Us"
+				breadcrumbUrl="/"
+				bgImage=""
+			/>
 
-      <ContactForm />
+			<ContactForm/>
 
-      <Footer />
-    </>
-  );
+			<Footer/>
+		</>
+	);
 };
 
 export default Contact;

fd53c1e568ad07b994007ca759bebe7cf08f1464
miao.zhang1 <miao.zhang1@ucalgary.ca>
2022-11-09 20:18:57 -0700

    Add meta data in data driven approach

--- pages/data-driven-approach.js
+++ pages/data-driven-approach.js
@@ -3,10 +3,23 @@ import Navbar from "../components/Layouts/Navbar";
 import PageBanner from "../components/Common/PageBanner";
 import Footer from "../components/Layouts/Footer";
 import DataDrivenApproachContent from "../components/About/DataDrivenApproachContent";
+import Head from "next/head";
 
 const DataDrivenApproach = () => {
 	return (
 		<>
+			<Head>
+				<title>EnviroByte Inc. | Data Driven Approach</title>
+				<meta name="keywords" content={"envirobyte Environment GHG Calgary Alberta Data Driven Approach"} />
+				<meta property="og:title" content="EnviroByte Inc. - Data Driven Approach" />
+				<meta property="og:PageType" content="website" />
+				<meta property="og:type" content="website" />
+				<meta property="og:site_name" content="https://www.envirobyte.com" />
+				<meta property="og:url" content="https://www.envirobyte.com/pages/service/data-driven-approach.js" />
+				<meta property="og:locale" content="en" />
+				<meta name="viewport" content="width=device-width,initial-scale=1.0" />
+				<meta name="description" content="Envirobyte services are here to help you and your business...Official web site of EnviroByte, located in Calgary, Alberta, Canada." />
+			</Head>
 			<Navbar />
 
 			<PageBanner

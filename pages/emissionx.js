bd5175668cc7a9f57278846959953996fa630b87
Minxing <minxingsi@gmail.com>
2022-11-11 09:04:38 -0700

    update emission X page

--- pages/emissionx.js
+++ pages/emissionx.js
@@ -8,45 +8,50 @@ import Head from "next/head";
 import Footer from "../components/Layouts/Footer";
 
 const EmissionX = () => {
-    return (
-        <>
-            <Head><title>EnviroByte Inc. | EmissionX</title>
-                <meta name="keywords" content={"envirobyte Environment GHG Calgary Alberta EmissionX"}/>
-                <meta property="og:title" content="EnviroByte Inc. - EmissionX"/>
-                <meta property="og:PageType" content="website"/>
-                <meta property="og:type" content="website"/>
-                <meta property="og:site_name" content="https://www.envirobyte.com"/>
-                <meta property="og:url" content="https://www.envirobyte.com/pages/emissionx.js"/>
-                <meta property="og:locale" content="en"/>
-                <meta name="twitter:site" content="@envirobyte"/>
-                <meta property="twitter:title" content="EnviroByte Inc. - About Us"/>
-                <meta property="twitter:url" content="https://www.envirobyte.com/pages/emissionx.js"/>
-                <meta name="viewport" content="width=device-width,initial-scale=1.0" />
-                <meta name="description" content="Envirobyte services are here to help you and your business...Official web site of EnviroByte, located in Calgary, Alberta, Canada." />
-            </Head>
-            <Navbar />
+  return (
+    <>
+      <Head>
+        <title>EnviroByte Inc. | EmissionX</title>
+        <meta
+          name="keywords"
+          content={"envirobyte Environment GHG Calgary Alberta EmissionX"}
+        />
+        <meta property="og:title" content="EnviroByte Inc. - EmissionX" />
+        <meta property="og:PageType" content="website" />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="https://www.envirobyte.com" />
+        <meta
+          property="og:url"
+          content="https://www.envirobyte.com/pages/emissionx.js"
+        />
+        <meta property="og:locale" content="en" />
+        <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+        <meta
+          name="description"
+          content="Envirobyte services are here to help you and your business...Official web site of EnviroByte, located in Calgary, Alberta, Canada."
+        />
+      </Head>
+      <Navbar />
 
-            <PageBanner
-                pageTitle="EmissionX"
-                breadcrumbTextOne="Home"
-                breadcrumbTextTwo="EmissionX"
-                breadcrumbUrl="/"
-                bgImage="/images/page-title-bg.jpg"
-            />
+      <PageBanner
+        pageTitle="EmissionX"
+        breadcrumbTextOne="Home"
+        breadcrumbTextTwo="EmissionX"
+        breadcrumbUrl="/"
+        bgImage=""
+      />
 
-            <EmissionXContent />
+      <EmissionXContent />
 
-            <FunFacts />
+      <FunFacts />
 
-            <div className="pb-100">
-                <GetStartedProject />
-            </div>
+      <div className="pb-100">
+        <GetStartedProject />
+      </div>
 
-
-
-            <Footer />
-        </>
-    );
+      <Footer />
+    </>
+  );
 };
 
 export default EmissionX;

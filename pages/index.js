0206ddb2d917a31913153e82e02534c91c45cdba
Minxing <minxingsi@gmail.com>
2022-11-15 06:21:41 -0700

    remove twitter from index.js

--- pages/index.js
+++ pages/index.js
@@ -11,20 +11,27 @@ import Head from "next/head";
 const Index = () => {
   return (
     <>
-      <Head><title>EnviroByte Inc. | Home</title>
-            <meta name="keywords" content={"envirobyte Environment GHG Calgary calgary Alberta"}/>
-            <meta property="og:title" content="EnviroByte Inc. - Home Page"/>
-            <meta property="og:PageType" content="website"/>
-            <meta property="og:type" content="website"/>
-            <meta property="og:site_name" content="https://www.envirobyte.com"/>
-            <meta property="og:url" content="https://www.envirobyte.com/pages/index.js"/>
-            <meta property="og:locale" content="en"/>
-            <meta name="twitter:site" content="@envirobyte"/>
-            <meta property="twitter:title" content="EnviroByte Inc. - Home Page"/>
-            <meta property="twitter:url" content="https://www.envirobyte.com/pages/index.js"/>
-            <meta name="viewport" content="width=device-width,initial-scale=1.0" />
-            <meta name="description" content="Envirobyte services are here to help you and your business...Official web site of EnviroByte, located in Calgary, Alberta, Canada." />
-        </Head>
+      <Head>
+        <title>EnviroByte Inc. | Home</title>
+        <meta
+          name="keywords"
+          content={"envirobyte Environment GHG Calgary calgary Alberta"}
+        />
+        <meta property="og:title" content="EnviroByte Inc. - Home Page" />
+        <meta property="og:PageType" content="website" />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="https://www.envirobyte.com" />
+        <meta
+          property="og:url"
+          content="https://www.envirobyte.com/pages/index.js"
+        />
+        <meta property="og:locale" content="en" />
+        <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+        <meta
+          name="description"
+          content="Envirobyte services are here to help you and your business...Official web site of EnviroByte, located in Calgary, Alberta, Canada."
+        />
+      </Head>
 
       <Navbar />
 

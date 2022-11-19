c9f22c461271160a967ea6baeb4c051bce80df13
miao.zhang1 <miao.zhang1@ucalgary.ca>
2022-11-19 11:59:40 -0700

    Update data driven approach content.

--- components/About/DataDrivenApproachContent.js
+++ components/About/DataDrivenApproachContent.js
@@ -6,134 +6,52 @@ const DataDrivenApproachContent = () => {
     <>
       <div className="project-details-area ptb-100">
         <div className="container">
-          <div className="project-details-image">
-            <img src="/images/projects/Data-Framework.svg" alt="image" />
-          </div>
 
           <div className="projects-details-desc">
-            <h3>We Provide Useful Services</h3>
+            <h3>Data-Driven Sustainability Framework</h3>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
+              Data-driven sustainability is the process of making decisions that are centered on sustainability and are
+              informed by the collecting, processing, and analysis of data. The data is then visualized to produce a
+              greater, significant impact and more ethical business practices. Insights from sustainability data can
+              create huge improvement while boosting revenue, whether it's reducing greenhouse gas emissions, improving
+              supply chains, or reducing waste.
             </p>
 
             <div className="features-text">
-              <h4>Core Development</h4>
-              <p>
-                No fake products and services. The customer is king, their lives
-                and needs are the inspiration. Sed ut perspiciatis unde omnis
-                iste natus error sit voluptatem accusantium doloremque
-                laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore
-                veritatis et quasi architecto beatae vitae dicta sunt explicabo.
-                Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit
-                aut fugit, sed quia consequuntur magni dolores eos qui ratione
-                voluptatem sequi nesciunt.
-              </p>
-            </div>
-
-            <div className="features-text">
-              <h4>Define Your Choices</h4>
-              <p>
-                No fake products and services. The customer is king, their lives
-                and needs are the inspiration. But I must explain to you how all
-                this mistaken idea of denouncing pleasure and praising pain was
-                born and I will give you a complete account of the system, and
-                expound the actual teachings of the great explorer of the truth,
-                the master-builder of human happiness. No one rejects, dislikes,
-                or avoids pleasure itself, because it is pleasure, but because
-                those who do not know how to pursue pleasure rationally
-                encounter consequences that are extremely painful. Nor again is
-                there anyone who loves or pursues or desires
-              </p>
-            </div>
-
-            <p>
-              At vero eos et accusamus et iusto odio dignissimos ducimus qui
-              blanditiis praesentium voluptatum deleniti atque corrupti quos
-              dolores et quas molestias excepturi sint occaecati cupiditate non
-              provident, similique sunt in culpa qui officia deserunt mollitia
-              animi, id est laborum et dolorum fuga. Et harum quidem rerum
-              facilis est et expedita distinctio. Nam libero tempore, cum soluta
-              nobis est eligendi optio cumque nihil impedit quo minus id quod
-              maxime placeat facere possimus, omnis voluptas assumenda est,
-              omnis dolor repellendus. Temporibus autem quibusdam et aut
-              officiis debitis aut rerum necessitatibus saepe eveniet ut et
-              voluptates repudiandae sint et molestiae non recusandae. Itaque
-              earum rerum hic tenetur a sapiente delectus, ut aut reiciendis
-              voluptatibus.
-            </p>
-
-            <p>
-              On the other hand, we denounce with righteous indignation and
-              dislike men who are so beguiled and demoralized by the charms of
-              pleasure of the moment, so blinded by desire, that they cannot
-              foresee the pain and trouble that are bound to ensue; and equal
-              blame belongs to those who fail in their duty through weakness of
-              will, which is the same as saying through shrinking from toil and
-              pain. These cases are perfectly simple and easy to distinguish. In
-              a free hour, when our power of choice is untrammelled and when
-              nothing prevents our being able to do what we like best, every
-              pleasure is to be welcomed and every pain avoided. But in certain
-              circumstances and owing to the claims of duty or the obligations
-              of business it will frequently occur that pleasures have to be
-              repudiated and annoyances accepted. The wise man therefore always
-              holds in these matters to this principle of selection: he rejects
-              pleasures to secure.
-            </p>
-
-            <div className="project-details-info">
-              <div className="single-info-box">
-                <h4>Client</h4>
-                <span>James Anderson</span>
-              </div>
-
-              <div className="single-info-box">
-                <h4>Category</h4>
-                <span>Network, Marketing</span>
-              </div>
-
-              <div className="single-info-box">
-                <h4>Date</h4>
-                <span>February 28, 2020</span>
-              </div>
-
-              <div className="single-info-box">
-                <h4>Share</h4>
-                <ul className="social">
+              <text>
+                The three major stages to follow when using a data-driven sustainability strategy are:
+                <p></p>
+                <ul>
                   <li>
-                    <a href="https://www.facebook.com/" target="_blank">
-                      <i className="fab fa-facebook-f"></i>
-                    </a>
+                    Establish industry-specific sustainability objectives: It is essential to have an industry-led
+                    strategy.
                   </li>
                   <li>
-                    <a href="https://twitter.com/" target="_blank">
-                      <i className="fab fa-twitter"></i>
-                    </a>
+                    Create a framework using artificial intelligence and machine learning so that businesses may use
+                    data modelling and forecasting methods to examine how current processes relate to sustainability
+                    objectives.
                   </li>
                   <li>
-                    <a href="https://www.linkedin.com/" target="_blank">
-                      <i className="fab fa-linkedin-in"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="https://www.instagram.com/" target="_blank">
-                      <i className="fab fa-instagram"></i>
-                    </a>
+                    Combine human and machine intelligence: machine learning takes over many data processing duties,
+                    revealing insights quickly so that teams can make sure data-driven decisions.
                   </li>
                 </ul>
-              </div>
-
-              <div className="single-info-box">
-                <a href="#" className="default-btn" target="_blank">
-                  Live Preview
-                </a>
-              </div>
+              </text>
             </div>
+
+            <div className="features-text">
+              <p>
+                A growing amount of pressure is being placed on organizations to manage their sustainability performance
+                as a result of reliable data gathering, reporting, and analytics. In order to empower industries to make
+                strategic, real-time decisions to meet sustainability goals (i.e Emission monitoring, GHG reporting and
+                reduction etc), we apply data-driven sustainability frameworks to emission monitoring, reporting, and
+                reduction through automatic data gathering, processing, and analytics.
+              </p>
+            </div>
+
+          </div>
+          <div className="project-details-image">
+            <img src="/images/projects/Data-Framework.svg" alt="image" />
           </div>
         </div>
       </div>

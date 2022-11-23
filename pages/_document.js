2a933de6527abfbe66ea8815a5a6a8f5655ba251
miao.zhang1 <miao.zhang1@ucalgary.ca>
2022-11-23 13:31:56 -0700

    Edit favicon

--- pages/_document.js
+++ pages/_document.js
@@ -21,7 +21,7 @@ class MyDocument extends Document {
             rel="stylesheet"
           />
           {/*<link rel="icon" type="image/png" href="/favicon.png"></link>*/}
-          <link rel="icon" href="/Favicon_20221106.png" sizes="any"></link>
+          <link rel="icon" href="/Favicon_20221121.png" sizes="any"></link>
           <link rel="icon" href="/icon.svg" type="image/svg+xml"></link>
           <link rel="apple-touch-icon" href="/apple-touch-icon.png"></link>
           <link rel="manifest" href="../site.webmanifest"></link>

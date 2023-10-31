65701f41b33b6de8e88bf129726bdddf17d18ab1
miao.zhang1 <miao.zhang1@ucalgary.ca>
2023-10-31 15:13:34 -0600

    try relative path

--- pages/_app.js
+++ pages/_app.js
@@ -16,16 +16,16 @@ import Head from "next/head";
 import GoTop from "../components/Shared/GoTop";
 import Script from "next/script";
 
-function MyApp({ Component, pageProps }) {
-  React.useEffect(() => {
-    AOS.init();
-  }, []);
-  return (
-    <>
-      <Head>
-          {/* Global Site Tag (gtag.js) - Google Analytics */}
-          <Script id="google-analytics" strategy="afterInteractive">
-              {`
+function MyApp({Component, pageProps}) {
+	React.useEffect(() => {
+		AOS.init();
+	}, []);
+	return (
+		<>
+			<Head>
+				{/* Global Site Tag (gtag.js) - Google Analytics */}
+				<Script id="google-analytics" strategy="afterInteractive">
+					{`
           				(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
           				(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
           				m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -34,19 +34,19 @@ function MyApp({ Component, pageProps }) {
           				ga('create', 'G-RMCG4QY4VP', 'auto');
           				ga('send', 'pageview');
         			`}
-          </Script>
-        <title>EnviroByte</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-		  <meta property="og:image" content="https://www.envirobyte.com/Favicon_20221121.png"/>
+				</Script>
+				<title>EnviroByte</title>
+				<meta name="viewport" content="initial-scale=1.0, width=device-width"/>
+				<meta property="og:image" content="/Favicon_20221121.png"/>
 
-      </Head>
+			</Head>
 
-      <Component {...pageProps} />
+			<Component {...pageProps} />
 
-      {/* Go Top Button */}
-      <GoTop />
-    </>
-  );
+			{/* Go Top Button */}
+			<GoTop/>
+		</>
+	);
 }
 
 export default MyApp;

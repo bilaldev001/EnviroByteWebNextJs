f52dd9b25149a9b571956f35ed645737b2cdeaba
Minxing <minxingsi@gmail.com>
2023-12-21 22:19:57 -0700

    add vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,10 @@ import React from "react";
 import AOS from "aos";
 import "../node_modules/aos/dist/aos.css";
 import "../styles/bootstrap.min.css";
-import '../styles/flaticon.css';
-import '../styles/fontawesome.min.css';
+import "../styles/flaticon.css";
+import "../styles/fontawesome.min.css";
 import "react-accessible-accordion/dist/fancy-example.css";
-import 'react-tabs/style/react-tabs.css';
+import "react-tabs/style/react-tabs.css";
 import "swiper/css/bundle";
 
 // Globals CSS
@@ -15,6 +15,7 @@ import "../styles/responsive.css";
 import Head from "next/head";
 import GoTop from "../components/Shared/GoTop";
 import Script from "next/script";
+import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps }) {
   React.useEffect(() => {
@@ -23,9 +24,9 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-          {/* Global Site Tag (gtag.js) - Google Analytics */}
-          <Script id="google-analytics" strategy="afterInteractive">
-              {`
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
           				(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
           				(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
           				m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -34,13 +35,13 @@ function MyApp({ Component, pageProps }) {
           				ga('create', 'G-RMCG4QY4VP', 'auto');
           				ga('send', 'pageview');
         			`}
-          </Script>
+        </Script>
         <title>EnviroByte</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
 
       <Component {...pageProps} />
-
+      <Analytics />
       {/* Go Top Button */}
       <GoTop />
     </>

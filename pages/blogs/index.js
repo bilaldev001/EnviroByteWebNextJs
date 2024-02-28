6c552cec4c2d3b4f90dd6f122b13f2253e1fa10d
bilal060 <ahmad_4al@hotmail.com>
2024-02-28 15:57:48 +0500

    integrated pagination

--- pages/blogs/index.js
+++ pages/blogs/index.js
@@ -3,8 +3,7 @@ import Head from "next/head";
 import BlogsComponent from "../../components/Blogs/BlogsComponent";
 import withMainLayout from "../../components/Layouts";
 
-const Blogs = ({ posts }) => {
-
+const Blogs = ({ posts, totalPages, currentPage }) => {
   return (
     <div>
       <Head>
@@ -27,35 +26,36 @@ const Blogs = ({ posts }) => {
           content="EnviroByte uses CI/CD to automate the process of calculating GHG emissions for Alberta TIER, BC GHG regulation, ECCC GHGRP, ECCC NPRI."
         />
       </Head>
-      {/* <PageBanner
-        pageTitle="Blogs"
-        breadcrumbTextOne="Home"
-        breadcrumbTextTwo="Blogs"
-        breadcrumbUrl="/"
-        bgImage=""
-      /> */}
-      <BlogsComponent posts={posts} />
+      <BlogsComponent
+        posts={posts}
+        totalPages={totalPages}
+        currentPage={currentPage}
+      />
     </div>
   );
 };
 
-export async function getStaticProps() {
-  const page = 1; 
-  const perPage = 10; 
+export async function getServerSideProps({ query }) {
+  const page = query.page || 1;
+  const perPage = 1;
 
-  const response = await fetch(`${process.env.BACKEND_URL}/api/blogs/getBlogs?page=${page}&perPage=${perPage}`);
+  const response = await fetch(
+    `${process.env.BACKEND_URL}/api/blogs/getBlogs?page=${page}&perPage=${perPage}`
+  );
   if (!response.ok) {
     throw new Error(`HTTP error! Status: ${response.status}`);
   }
-  
+
   const data = await response.json();
   const { blogs, totalPages } = data;
+  const currentPage = +page;
 
   if (!data || blogs?.length === 0) {
     return {
       props: {
         posts: [],
         totalPages: 0,
+        currentPage,
       },
     };
   }
@@ -64,6 +64,7 @@ export async function getStaticProps() {
     props: {
       posts: blogs,
       totalPages,
+      currentPage,
     },
   };
 }

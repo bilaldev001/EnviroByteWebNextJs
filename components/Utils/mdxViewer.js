d2046ee093cbd4c9e47c46dd7163bf6315551ba6
Muhammad Bilal <ahmad_4al@hotmail.com>
2024-03-01 12:08:20 +0500

    fix mdx files

--- components/Utils/mdxViewer.js
+++ components/Utils/mdxViewer.js
@@ -1,24 +1,31 @@
 import React from 'react';
-
-// Create a functional component to render MDX content
-const MDXRenderer = ({ content }) => {
-  // Split the MDX content into paragraphs
-  const paragraphs = content.split('\n').map((paragraph, index) => (
-    <p key={index}>{paragraph}</p>
-  ));
-
-  return (
-    <div>
-      {paragraphs}
-    </div>
-  );
-};
+import ReactMarkdown from 'react-markdown';
 
 // Create your main component where you render MDX content
 const MdxViewer = ({ mdxSource }) => {
   return (
     <div>
-      <MDXRenderer content={mdxSource} />
+      <ReactMarkdown
+        components={{
+          // Render links as anchor tags
+          a: ({ node, ...props }) => <a {...props} target="_blank" rel="noopener noreferrer" />,
+          // Render code blocks with proper formatting
+          code: ({ node, inline, className, children, ...props }) => {
+            const match = /language-(\w+)/.exec(className || '');
+            return !inline && match ? (
+              <pre className={className}>
+                <code {...props}>{children}</code>
+              </pre>
+            ) : (
+              <code className={className} {...props}>
+                {children}
+              </code>
+            );
+          },
+        }}
+      >
+        {mdxSource}
+      </ReactMarkdown>
     </div>
   );
 };
